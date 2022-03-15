12ccf5755426021065ec3380517b0b1c7481150c
devming <cmk330@gmail.com>
2022-03-15 21:34:57 +0900

    샘플 플래피 마무리

---
+++ Assets/BirdJump.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdJump : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpPower = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().Play();
+            rb.velocity = Vector2.up * jumpPower; // (0, 3)  == y 좌표 방향으로 3만큼 점프
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Score.score > Score.bestScore)
+        {
+            Score.bestScore = Score.score;
+        }
+        SceneManager.LoadScene("GameOverScene");
+    }
+}
